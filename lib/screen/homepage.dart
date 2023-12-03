a375d36562bdea5b80332b7acf52c35145c02c5d
devsTz <rogashianmvungi@gmail.com>
2023-12-03 21:09:48 +0300

    Bottom Navigation Bar

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -9,6 +9,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _indexSelected = 0;
+  static const List<Widget> _pages = <Widget>[
+    Center(
+      child: Text('Page 01'),
+    ),
+    Center(
+      child: Text('Page 02'),
+    ),
+    Center(
+      child: Text('Page 03'),
+    ),
+    Center(
+      child: Text('Page 04'),
+    ),
+  ];
+  @override
+  void _onItemTapped(int index) {
+    setState(() {
+      _indexSelected = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +43,35 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _indexSelected,
+        backgroundColor: Colors.deepOrange,
+        iconSize: 20,
+        fixedColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.sports_baseball_rounded,
+              ),
+              label: 'Soccer'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_rounded), label: 'Message'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_rounded), label: 'Profile'),
+        ],
+      ),
       // drawer: Drawer(
       //   backgroundColor: Colors.red,
       // ),
+      body: Center(
+        child: _pages.elementAt(_indexSelected),
+      ),
     );
   }
 }
